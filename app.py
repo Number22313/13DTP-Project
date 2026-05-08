17f6ca8963c3eaf7bf8e32d30b7e12e72e043181
Number22313 <22313@burnside.school.nz>
2026-05-08 12:46:06 +1200

    Started database tables (part combinations and parts tables)

--- app.py
+++ app.py
@@ -2,3 +2,13 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String, Text, ForeignKey
 from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
