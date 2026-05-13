123dbb728e8ef10f2f259972e2ed5ad7f4bc1984
Number22313 <22313@burnside.school.nz>
2026-05-14 09:25:15 +1200

    Started adding the python class tables

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 
-
 class Setups(db.Model):
     __tablename__ = "Setups"
     setup_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -18,14 +17,21 @@ class Setups(db.Model):
     tune_id = db.Column(db.Integer, db.ForeignKey("Tunes.tune_id"), nullable=False)
     vehicle_id = db.Column(db.Integer, db.ForeignKey("Vehicles.vehicle_id"), nullable=False)
     part_id = db.Column(db.Integer, db.ForeignKey("'Part Combinations'.part_id"), nullable=False)
+    wr_time = db.relationship("WR_Times")
+    track = db.relationship("Tracks")
+    tune = db.relationship("Tunes")
+    vehicle = db.relationship("Vehicles")
+    parts_combinations = db.relationship(" 'Part Combinations' ")
 
-class WR_Times(db.model):
+class WR_Times(db.Model):
     __tablename__ = "WR_Times"
-    time_id = db.Column(db.Integer, db.)
+    time_id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    time = db.Column(db.REAL, nullable=False)
+    player = db.Column(db.Text, nullable=False)
 
 @app.route('/')
 def home():
-    setup = Setups.query.all()
+    setup = db.session.get(Setups, 1)
     return render_template('home.html', setup=setup)
 
 if __name__ == '__main__':
