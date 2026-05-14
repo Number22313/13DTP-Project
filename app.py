9c3da492f7638111f657377e9223af9a712563f8
Number22313 <22313@burnside.school.nz>
2026-05-15 11:44:14 +1200

    Displays multiple setups on the home page in a table

--- app.py
+++ app.py
@@ -16,12 +16,12 @@ class Setups(db.Model):
     track_id = db.Column(db.Integer, db.ForeignKey("Tracks.track_id"), nullable=False)
     tune_id = db.Column(db.Integer, db.ForeignKey("Tunes.tune_id"), nullable=False)
     vehicle_id = db.Column(db.Integer, db.ForeignKey("Vehicles.vehicle_id"), nullable=False)
-    part_id = db.Column(db.Integer, db.ForeignKey("'Part Combinations'.part_id"), nullable=False)
+    part_id = db.Column(db.Integer, db.ForeignKey("Part Combinations.part_id"), nullable=False)
     wr_time = db.relationship("WR_Times")
     track = db.relationship("Tracks")
     tune = db.relationship("Tunes")
     vehicle = db.relationship("Vehicles")
-    parts_combinations = db.relationship(" 'Part Combinations' ")
+    parts_combinations = db.relationship("Parts")
 
 class WR_Times(db.Model):
     __tablename__ = "WR_Times"
@@ -29,10 +29,36 @@ class WR_Times(db.Model):
     time = db.Column(db.REAL, nullable=False)
     player = db.Column(db.Text, nullable=False)
 
+class Tracks(db.Model):
+    __tablename__ = "Tracks"
+    track_id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False, unique=True)
+    track_name = db.Column(db.Text, nullable=False)
+
+class Tunes(db.Model):
+    __tablename__ = "Tunes"
+    tune_id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False, unique=True)
+    tune1 = db.Column(db.Integer, nullable=False)
+    tune2 = db.Column(db.Integer, nullable=False)
+    tune3 = db.Column(db.Integer, nullable=False)
+    tune4 = db.Column(db.Integer, nullable=False)
+
+class Vehicles(db.Model):
+    __tablename__ = "Vehicles"
+    vehicle_id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False, unique=True)
+    vehicle_name = db.Column(db.Text, unique=True)
+
+class Parts(db.Model):
+    __tablename__ = "Part Combinations"
+    part_id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False, unique=True)
+    slot1 = db.Column(db.Text, nullable=False)
+    slot2 = db.Column(db.Text, nullable=False)
+    slot3 = db.Column(db.Text, nullable=False)
+
+
 @app.route('/')
 def home():
-    setup = db.session.get(Setups, 1)
-    return render_template('home.html', setup=setup)
+    setups = Setups.query.all()
+    return render_template('home.html', setups=setups)
 
 if __name__ == '__main__':
     app.run(debug=True)
